1f999c72ad1d7fea8400a4bcc4e6e39b903128d2
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-10-10 01:32:39 +0300

    Store non-fs items data into dav_properties

--- apps/dav/lib/Files/CustomPropertiesBackend.php
+++ apps/dav/lib/Files/CustomPropertiesBackend.php
@@ -26,6 +26,7 @@ use OCA\DAV\Connector\Sabre\Directory;
 use OCA\DAV\Connector\Sabre\Node;
 use OCP\IDBConnection;
 use OCP\IUser;
+use Sabre\DAV\INode;
 use Sabre\DAV\PropertyStorage\Backend\BackendInterface;
 use Sabre\DAV\PropFind;
 use Sabre\DAV\PropPatch;
@@ -35,6 +36,26 @@ use Sabre\DAV\Exception\NotFound;
 use Sabre\DAV\Exception\ServiceUnavailable;
 
 class CustomPropertiesBackend implements BackendInterface {
+
+	const SELECT_BY_ID_STMT = 'SELECT * FROM `*PREFIX*properties` WHERE `fileid` = ?';
+	const INSERT_BY_ID_STMT = 'INSERT INTO `*PREFIX*properties`'
+			. ' (`fileid`,`propertyname`,`propertyvalue`) VALUES(?,?,?)';
+	const UPDATE_BY_ID_AND_NAME_STMT = 'UPDATE `*PREFIX*properties`'
+			. ' SET `propertyvalue` = ? WHERE `fileid` = ? AND `propertyname` = ?';
+	const DELETE_BY_ID_STMT = 'DELETE FROM `*PREFIX*properties` WHERE `fileid` = ?';
+	const DELETE_BY_ID_AND_NAME_STMT = 'DELETE FROM `*PREFIX*properties`'
+			. ' WHERE `fileid` = ? AND `propertyname` = ?';
+
+	const SELECT_BY_PATH_STMT = 'SELECT * FROM `*PREFIX*dav_properties` WHERE `propertypath` = ?';
+	const INSERT_BY_PATH_STMT = 'INSERT INTO `*PREFIX*dav_properties`'
+			. ' (`propertypath`, `propertyname`, `propertyvalue`) VALUES(?,?,?)';
+	const UPDATE_BY_PATH_STMT = 'UPDATE `*PREFIX*dav_properties`'
+			. ' SET `propertypath` = ? WHERE `propertypath` = ?';
+	const UPDATE_BY_PATH_AND_NAME_STMT = 'UPDATE `*PREFIX*dav_properties` '
+			. 'SET `propertyvalue` = ? WHERE `propertypath` = ? AND `propertyname` = ?';
+	const DELETE_BY_PATH_STMT = 'DELETE FROM `*PREFIX*dav_properties` WHERE `propertypath` = ?';
+	const DELETE_BY_PATH_AND_NAME_STMT = 'DELETE FROM `*PREFIX*dav_properties`'
+			. ' WHERE `propertypath` = ? AND `propertyname` = ?';
 
 	/**
 	 * Ignored properties
@@ -69,11 +90,18 @@ class CustomPropertiesBackend implements BackendInterface {
 	private $user;
 
 	/**
-	 * Properties cache
+	 * Property cache for the filesystem items
 	 *
 	 * @var array
 	 */
-	private $cache = [];
+	private $cacheByFileId = [];
+
+	/**
+	 * Property cache for the non-filesystem items
+	 *
+	 * @var array
+	 */
+	private $cacheByPath = [];
 
 	/**
 	 * @param Tree $tree node tree
@@ -97,29 +125,8 @@ class CustomPropertiesBackend implements BackendInterface {
 	 * @return void
 	 */
 	public function propFind($path, PropFind $propFind) {
-		try {
-			$node = $this->tree->getNodeForPath($path);
-			if (!($node instanceof Node)) {
-				return;
-			}
-		} catch (ServiceUnavailable $e) {
-			// might happen for unavailable mount points, skip
-			return;
-		} catch (Forbidden $e) {
-			// might happen for excluded mount points, skip
-			return;
-		} catch (NotFound $e) {
-			// in some rare (buggy) cases the node might not be found,
-			// we catch the exception to prevent breaking the whole list with a 404
-			// (soft fail)
-			\OC::$server->getLogger()->warning(
-				'Could not get node for path: "{path}" : {$message}',
-				[
-					'app' => 'dav',
-					'path' => $path,
-					'message' => $e->getMessage(),
-				]
-			);
+		$node = $this->getNodeForPath($path);
+		if (is_null($node)) {
 			return;
 		}
 
@@ -142,7 +149,7 @@ class CustomPropertiesBackend implements BackendInterface {
 			$this->loadChildrenProperties($node, $requestedProps);
 		}
 
-		$props = $this->getProperties($node, $requestedProps);
+		$props = $this->getProperties($path, $node, $requestedProps);
 		foreach ($props as $propName => $propValue) {
 			$propFind->set($propName, $propValue);
 		}
@@ -157,13 +164,13 @@ class CustomPropertiesBackend implements BackendInterface {
 	 * @return void
 	 */
 	public function propPatch($path, PropPatch $propPatch) {
-		$node = $this->tree->getNodeForPath($path);
-		if (!($node instanceof Node)) {
+		$node = $this->getNodeForPath($path);
+		if (is_null($node)) {
 			return;
 		}
 
-		$propPatch->handleRemaining(function($changedProps) use ($node) {
-			return $this->updateProperties($node, $changedProps);
+		$propPatch->handleRemaining(function($changedProps) use ($path, $node) {
+			return $this->updateProperties($path, $node, $changedProps);
 		});
 	}
 
@@ -173,15 +180,23 @@ class CustomPropertiesBackend implements BackendInterface {
 	 * @param string $path path of node for which to delete properties
 	 */
 	public function delete($path) {
-		$fileId = $this->getFileIdByPath($path);
-		if ($fileId) {
-			$statement = $this->connection->prepare(
-				'DELETE FROM `*PREFIX*properties` WHERE `fileid` = ?'
-			);
-			$statement->execute([$fileId]);
-			$statement->closeCursor();
-			unset($this->cache[$fileId]);
+		$node = $this->getNodeForPath($path);
+		if (is_null($node)) {
+			return;
 		}
+
+		if ($node instanceof Node) {
+			$fileId = $node->getId();
+			$statement = $this->connection->prepare(self::DELETE_BY_ID_STMT);
+			$statement->execute([$fileId]);
+			unset($this->cacheByFileId[$fileId]);
+		} else {
+			$statement = $this->connection->prepare(self::DELETE_BY_PATH_STMT);
+			$statement->execute([$path]);
+			unset($this->cacheByPath[$path]);
+		}
+
+		$statement->closeCursor();
 	}
 
 	/**
@@ -193,12 +208,25 @@ class CustomPropertiesBackend implements BackendInterface {
 	 * @return void
 	 */
 	public function move($source, $destination) {
-		// Do nothing
+		$node = $this->getNodeForPath($source);
+		if (is_null($node)) {
+			return;
+		}
+
+		if ($node instanceof Node) {
+			// Do nothing
+		} else {
+			$statement = $this->connection->prepare(self::UPDATE_BY_PATH_STMT);
+			$statement->execute([$destination, $source]);
+			$statement->closeCursor();
+		}
 	}
 
 	/**
-	 * Returns a list of properties for this nodes.;
-	 * @param Node $node
+	 * Returns a list of properties for these nodes
+	 *
+	 * @param string $path path
+	 * @param INode $node
 	 * @param array $requestedProperties requested properties or empty array for "all"
 	 * @return array
 	 * @note The properties list is a list of propertynames the client
@@ -206,25 +234,83 @@ class CustomPropertiesBackend implements BackendInterface {
 	 * http://www.example.org/namespace#author If the array is empty, all
 	 * properties should be returned
 	 */
-	private function getProperties(Node $node, array $requestedProperties) {
-		$fileId = $node->getId();
-		if (isset($this->cache[$fileId])) {
-			return $this->cache[$fileId];
+	private function getProperties($path, INode $node, array $requestedProperties) {
+		if ($node instanceof Node) {
+			$fileId = $node->getId();
+			return $this->getPropertiesById($fileId, $requestedProperties);
+		} else {
+			return $this->getPropertiesByPath($path, $requestedProperties);
 		}
+	}
 
-		// TODO: chunking if more than 1000 properties
-		$sql = 'SELECT * FROM `*PREFIX*properties` WHERE `fileid` = ?';
+	/**
+	 * Returns a list of properties for nodes;
+	 * @param int $fileId
+	 * @param array $requestedProperties requested properties or empty array for "all"
+	 * @return array
+	 * @note The properties list is a list of propertynames the client
+	 * requested, encoded as xmlnamespace#tagName, for example:
+	 * http://www.example.org/namespace#author If the array is empty, all
+	 * properties should be returned
+	 */
+	private function getPropertiesById($fileId, array $requestedProperties) {
+		if (!isset($this->cacheByFileId[$fileId])) {
+			// TODO: chunking if more than 1000 properties
+			$sql = self::SELECT_BY_ID_STMT;
+			$whereValues = [$fileId];
+			$whereTypes = [null];
 
-		$whereValues = [$fileId];
-		$whereTypes = [null];
+			if (!empty($requestedProperties)) {
+				// request only a subset
+				$sql .= ' AND `propertyname` in (?)';
+				$whereValues[] = $requestedProperties;
+				$whereTypes[] = Connection::PARAM_STR_ARRAY;
+			}
 
-		if (!empty($requestedProperties)) {
-			// request only a subset
-			$sql .= ' AND `propertyname` in (?)';
-			$whereValues[] = $requestedProperties;
-			$whereTypes[] = Connection::PARAM_STR_ARRAY;
+			$props = $this->fetchProperties($sql, $whereValues, $whereTypes);
+			$this->cacheByFileId[$fileId] = $props;
 		}
+		return $this->cacheByFileId[$fileId];
+	}
 
+	/**
+	 * Returns a list of properties for nodes
+	 * @param string $path
+	 * @param array $requestedProperties requested properties or empty array for "all"
+	 * @return array
+	 * @note The properties list is a list of propertynames the client
+	 * requested, encoded as xmlnamespace#tagName, for example:
+	 * http://www.example.org/namespace#author If the array is empty, all
+	 * properties should be returned
+	 */
+	private function getPropertiesByPath($path, array $requestedProperties) {
+		if (!isset($this->cacheByPath[$path])) {
+			// TODO: chunking if more than 1000 properties
+			$sql = self::SELECT_BY_PATH_STMT;
+
+			$whereValues = [$path];
+			$whereTypes = [null];
+
+			if (!empty($requestedProperties)) {
+				// request only a subset
+				$sql .= ' AND `propertyname` in (?)';
+				$whereValues[] = $requestedProperties;
+				$whereTypes[] = Connection::PARAM_STR_ARRAY;
+			}
+
+			$props = $this->fetchProperties($sql, $whereValues, $whereTypes);
+			$this->cacheByPath[$path] = $props;
+		}
+		return $this->cacheByPath[$path];
+	}
+
+	/**
+	 * @param string $sql
+	 * @param array $whereValues
+	 * @param array $whereTypes
+	 * @return array
+	 */
+	private function fetchProperties($sql, $whereValues, $whereTypes) {
 		$result = $this->connection->executeQuery(
 			$sql,
 			$whereValues,
@@ -237,37 +323,45 @@ class CustomPropertiesBackend implements BackendInterface {
 		}
 
 		$result->closeCursor();
-
-		$this->cache[$fileId] = $props;
 		return $props;
 	}
 
 	/**
 	 * Update properties
 	 *
-	 * @param Node $node node for which to update properties
-	 * @param array $properties array of properties to update
-	 *
+	 * @param string $path
+	 * @param INode $node node for which to update properties
+	 * @param array $changedProperties array of properties to update
 	 * @return bool
 	 */
-	private function updateProperties($node, $properties) {
-		$fileId = $node->getId();
-		$deleteStatement = 'DELETE FROM `*PREFIX*properties`' .
-			' WHERE `fileid` = ? AND `propertyname` = ?';
+	private function updateProperties($path, INode $node, $changedProperties) {
+		$existingProperties = $this->getProperties($path, $node, []);
+		if ($node instanceof Node) {
+			$fileId = $node->getId();
+			return $this->updatePropertiesByFileId($fileId, $changedProperties, $existingProperties);
+		} else {
+			return $this->updatePropertiesByPath($path, $changedProperties, $existingProperties);
+		}
+	}
 
-		$insertStatement = 'INSERT INTO `*PREFIX*properties`' .
-			' (`fileid`,`propertyname`,`propertyvalue`) VALUES(?,?,?)';
-
-		$updateStatement = 'UPDATE `*PREFIX*properties` SET `propertyvalue` = ?' .
-			' WHERE `fileid` = ? AND `propertyname` = ?';
+	/**
+	 * @param int $fileId
+	 * @param array $changedProperties
+	 * @param array $existingProperties
+	 * @return bool
+	 */
+	private function updatePropertiesByFileId($fileId, $changedProperties, $existingProperties) {
+		$deleteStatement = self::DELETE_BY_ID_AND_NAME_STMT;
+		$insertStatement = self::INSERT_BY_ID_STMT;
+		$updateStatement = self::UPDATE_BY_ID_AND_NAME_STMT;
 
 		// TODO: use "insert or update" strategy ?
-		$existing = $this->getProperties($node, []);
 		$this->connection->beginTransaction();
-		foreach ($properties as $propertyName => $propertyValue) {
+		foreach ($changedProperties as $propertyName => $propertyValue) {
+			$propertyExists = array_key_exists($propertyName, $existingProperties);
 			// If it was null, we need to delete the property
 			if (is_null($propertyValue)) {
-				if (array_key_exists($propertyName, $existing)) {
+				if ($propertyExists) {
 					$this->connection->executeUpdate($deleteStatement,
 						[
 							$fileId,
@@ -276,7 +370,7 @@ class CustomPropertiesBackend implements BackendInterface {
 					);
 				}
 			} else {
-				if (!array_key_exists($propertyName, $existing)) {
+				if (!$propertyExists) {
 					$this->connection->executeUpdate($insertStatement,
 						[
 							$fileId,
@@ -297,7 +391,59 @@ class CustomPropertiesBackend implements BackendInterface {
 		}
 
 		$this->connection->commit();
-		unset($this->cache[$fileId]);
+		unset($this->cacheByFileId[$fileId]);
+
+		return true;
+	}
+
+	/**
+	 * @param string $path
+	 * @param array $changedProperties
+	 * @param array $existingProperties
+	 * @return bool
+	 */
+	private function updatePropertiesByPath($path, $changedProperties, $existingProperties) {
+		$deleteStatement = self::DELETE_BY_PATH_AND_NAME_STMT;
+		$insertStatement = self::INSERT_BY_PATH_STMT;
+		$updateStatement = self::UPDATE_BY_PATH_AND_NAME_STMT;
+
+		// TODO: use "insert or update" strategy ?
+		$this->connection->beginTransaction();
+		foreach ($changedProperties as $propertyName => $propertyValue) {
+			$propertyExists = array_key_exists($propertyName, $existingProperties);
+			// If it was null, we need to delete the property
+			if (is_null($propertyValue)) {
+				if ($propertyExists) {
+					$this->connection->executeUpdate($deleteStatement,
+						[
+							$path,
+							$propertyName
+						]
+					);
+				}
+			} else {
+				if (!$propertyExists) {
+					$this->connection->executeUpdate($insertStatement,
+						[
+							$path,
+							$propertyName,
+							$propertyValue
+						]
+					);
+				} else {
+					$this->connection->executeUpdate($updateStatement,
+						[
+							$propertyValue,
+							$path,
+							$propertyName
+						]
+					);
+				}
+			}
+		}
+
+		$this->connection->commit();
+		unset($this->cacheByPath[$path]);
 
 		return true;
 	}
@@ -313,7 +459,7 @@ class CustomPropertiesBackend implements BackendInterface {
 	private function loadChildrenProperties(Directory $node, $requestedProperties) {
 		$fileId = $node->getId();
 
-		if (isset($this->cache[$fileId])) {
+		if (isset($this->cacheByFileId[$fileId])) {
 			// we already loaded them at some point
 			return;
 		}
@@ -325,7 +471,7 @@ class CustomPropertiesBackend implements BackendInterface {
 			$childId = $childNode->getId();
 			if ($childId) {
 				$childrenIds[] = $childId;
-				$this->cache[$childId] = [];
+				$this->cacheByFileId[$childId] = [];
 			}
 		}
 
@@ -341,24 +487,38 @@ class CustomPropertiesBackend implements BackendInterface {
 		$props = [];
 		while ($row = $result->fetch()) {
 			$props[$row['propertyname']] = $row['propertyvalue'];
-			$this->cache[$row['fileid']] = $props;
+			$this->cacheByFileId[$row['fileid']] = $props;
 		}
 
 		$result->closeCursor();
 	}
 
 	/**
-	 * @param string $filePath
-	 * @return int | null
+	 * @param string $path
+	 * @return INode|null
 	 */
-	private function getFileIdByPath($filePath){
-		if (!$this->tree->nodeExists($filePath)) {
-			return;
+	private function getNodeForPath($path){
+		try {
+			$node = $this->tree->getNodeForPath($path);
+			return $node;
+		} catch (ServiceUnavailable $e) {
+			// might happen for unavailable mount points, skip
+		} catch (Forbidden $e) {
+			// might happen for excluded mount points, skip
+		} catch (NotFound $e) {
+			// in some rare (buggy) cases the node might not be found,
+			// we catch the exception to prevent breaking the whole list with a 404
+			// (soft fail)
+			\OC::$server->getLogger()->warning(
+				'Could not get node for path: "{path}" : {$message}',
+				[
+					'app' => 'dav',
+					'path' => $path,
+					'message' => $e->getMessage(),
+				]
+			);
 		}
-		$node = $this->tree->getNodeForPath($filePath);
-		if ($node instanceof Node) {
-			return $node->getId();
-		}
+		return null;
 	}
 
 }
